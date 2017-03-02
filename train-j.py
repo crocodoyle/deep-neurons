ff678b40ff490061e575552e1c7eaef98b42283a
jmorse <joshua.morse@mail.mcgill.ca>
2017-03-02 16:28:49 -0500

    CNN runs on 980 GTX

---
+++ train-j.py
@@ -0,0 +1,60 @@
+from keras.optimizers import SGD
+from keras.models import Sequential
+
+
+from sklearn.model_selection import StratifiedShuffleSplit
+
+import models
+import numpy as np
+
+import h5py
+
+def load_data():
+    f = h5py.File('deep-neurons.hdf5', 'r')
+
+    labels = f['labels']
+
+    sss_validation = StratifiedShuffleSplit(n_splits=1, test_size=0.3, random_state=42)
+    sss_test = StratifiedShuffleSplit(n_splits=1, test_size=0.5, random_state=42)
+
+    train_indices, validation_indices, test_indices = None, None, None
+
+    for train_index, validation_index in sss_validation.split(np.zeros(len(labels)), labels):
+        train_indices      = train_index
+        validation_indices = validation_index
+
+    # for validation_index, test_index in sss_test.split(np.zeros(len(labels[validation_indices])), labels[validation_indices]):
+    #     validation_indices = validation_index
+    #     test_indices       = test_index
+
+    print("training images:", len(train_index))
+    print("validation images:", len(validation_index))
+    # print("test_index:", len(test_index))
+
+
+    return train_indices, validation_indices
+
+if __name__ == "__main__":
+    f = h5py.File('deep-neurons.hdf5', 'r')
+
+    train_index, val_index = load_data()
+
+    images = f['images']
+    labels = f['labels']
+
+#    model = models.mlp(3)
+    model = models.cnn()
+
+    sgd = SGD(lr=1e-3, momentum=0.9, decay=1e-6, nesterov=True)
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=sgd,
+                  metrics=["accuracy"])
+#    x_train = np.reshape(np.asarray(images)[train_index,...], (len(np.asarray(images)[train_index,...]), 128*128))
+    x_train = np.reshape(np.asarray(images)[train_index],(593,128,128,1))
+#    x_train = np.asarray(images)[train_index]
+    y_train = np.asarray(labels)[train_index]
+
+    model.fit(x_train, y_train, nb_epoch=100)
+
+
