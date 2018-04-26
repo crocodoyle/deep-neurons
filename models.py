79097d2b658a5cca9eb34ea2b5921e387cbd8db1
Andrew Doyle <andrew.doyle@mcgill.ca>
2018-04-26 13:20:27 -0400

    bug

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential
 
-from keras.layers import Dense, Dropout, Activation, Convolution2D, MaxPooling2D, Flatten, BatchNormalization, SpatialDropout2D, Reshape, UpSampling2D
+from keras.layers import Dense, Dropout, Activation, Deconvolution2D, Convolution2D, MaxPooling2D, Flatten, BatchNormalization, SpatialDropout2D, Reshape, UpSampling2D
+from keras.layers.advanced_activations import LeakyReLU
 from keras.optimizers import SGD
 from keras.callbacks import ModelCheckpoint
 
@@ -74,69 +75,86 @@ def cnn():
 
 def discriminator():
     model = Sequential()
+    model.add(Convolution2D(16, 5, 5, subsample=(2, 2), border_mode='valid', input_shape=(128, 128, 1,)))
+    lay = LeakyReLU(alpha=0.2)
+    model.add(lay)
+    # model.add(SpatialDropout2D(0.5))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode='valid'))
+    lay2 = LeakyReLU(alpha=0.2)
+    model.add(lay2)
 
-    model.add(Convolution2D(16, 3, 3, border_mode='same', input_shape=(128, 128, 1)))
-    model.add(Activation('relu'))
-    model.add(SpatialDropout2D(0.2))
-    model.add(BatchNormalization())
-    model.add(Convolution2D(32, 3, 3, border_mode='same'))
-    model.add(Activation('relu'))
-    model.add(SpatialDropout2D(0.2))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode='valid'))
+    lay3 = LeakyReLU(alpha=0.2)
+    model.add(lay3)
 
-    model.add(Convolution2D(32, 3, 3, border_mode='same'))
-    model.add(Activation('relu'))
-    model.add(SpatialDropout2D(0.3))
-    model.add(BatchNormalization())
-    model.add(Convolution2D(32, 3, 3, border_mode='same'))
-    model.add(Activation('relu'))
-    model.add(SpatialDropout2D(0.3))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode='valid'))
+    lay4 = LeakyReLU(alpha=0.2)
+    model.add(lay4)
 
-    model.add(Convolution2D(64, 3, 3, border_mode='same'))
-    model.add(Activation('relu'))
-    model.add(SpatialDropout2D(0.4))
-    model.add(BatchNormalization())
-    model.add(Convolution2D(32, 3, 3, border_mode='same'))
-    model.add(Activation('relu'))
-    model.add(SpatialDropout2D(0.5))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode='valid'))
+    lay5 = LeakyReLU(alpha=0.2)
+    model.add(lay5)
 
+    model.add(Convolution2D(4, 1, 1, border_mode='valid'))
+
+    lay3 = LeakyReLU(alpha=0.2)
+    model.add(lay3)
+    model.add(Convolution2D(1, 1, 1, border_mode='valid'))
     model.add(Flatten())
-    model.add(Dense(256, init='uniform'))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-
-    model.add(Dense(256, init='uniform'))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-
-    model.add(Dense(1, init='uniform'))
-    model.add(Activation('softmax'))
-
     return model
-
 
 def generator():
     model = Sequential()
-    model.add(Dense(input_dim=100, output_dim=128*128))
-    model.add(Activation('tanh'))
-    model.add(Dense(128*7*7))
+    model.add(Dense(input_dim=100, output_dim=16*16))
+    lay1 = LeakyReLU(alpha=0.2)
+    model.add(lay1)
+    model.add(Dense(1*16*16))
+    # model.add(Dropout(0.5))
     model.add(BatchNormalization())
-    model.add(Activation('tanh'))
-    model.add(Reshape((128, 7, 7), input_shape=(128*7*7,)))
-    model.add(UpSampling2D(size=(2, 2)))
-    model.add(Convolution2D(64, 5, 5, border_mode='same'))
-    model.add(Activation('tanh'))
-    model.add(UpSampling2D(size=(2, 2)))
-    model.add(Convolution2D(1, 5, 5, border_mode='same'))
+
+    model.add(Reshape((16, 16, 1), input_shape=(1*16*16,)))
+
+    model.add(Deconvolution2D(128, 5, 5, output_shape=(None, 32, 32, 128), subsample=(2, 2), border_mode='same'))
+    lay2 = LeakyReLU(alpha=0.2)
+    model.add(lay2)
+    # model.add(SpatialDropout2D(0.5))
+    model.add(BatchNormalization())
+
+    model.add(Deconvolution2D(256, 5, 5, output_shape=(None, 128, 128, 256), subsample=(4, 4), border_mode='same'))
+    lay3 = LeakyReLU(alpha=0.2)
+    model.add(lay3)
+    # model.add(SpatialDropout2D(0.5))
+    model.add(BatchNormalization())
+
+    # model.add(Deconvolution2D(16, 5, 5, output_shape=(None, 128, 128, 16), subsample=(2, 2), border_mode='same'))
+    # lay4 = LeakyReLU(alpha=0.2)
+    # model.add(lay4)
+    # model.add(SpatialDropout2D(0.5))
+    # model.add(BatchNormalization())
+
+    # model.add(Convolution2D(16, 5, 5, border_mode='same'))
+    # model.add(Activation('tanh'))
+    # model.add(SpatialDropout2D(0.5))
+    # model.add(Convolution2D(1, 5, 5, border_mode='same'))
+    # model.add(Activation('tanh'))
+    # model.add(SpatialDropout2D(0.5))
+
+    model.add(Convolution2D(1, 1, 1, border_mode='same'))
     model.add(Activation('tanh'))
 
+    return model
 
 def gan(generator, discriminator):
+    print('GENERATOR:')
+    print(generator.summary())
+    print('DISCRIMINATOR:')
+    print(discriminator.summary())
 
     model = Sequential()
     model.add(generator)
+
+
     generator.trainable = False
     model.add(discriminator)
     return model
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from keras.optimizers import SGD
+from keras.optimizers import SGD, Adam
 from keras.models import Sequential
 
 import make_dataset
@@ -7,11 +7,15 @@ from sklearn.model_selection import StratifiedShuffleSplit
 
 import models
 import numpy as np
+import matplotlib.pyplot as plt
 
 import h5py
 
+
+input_file = 'E:/deep-neurons/deep-neurons.hdf5'
+
 def load_data():
-    f = h5py.File('E:/deep-neurons/deep-neurons.hdf5', 'r')
+    f = h5py.File(input_file, 'r')
 
     labels = f['labels']
 
@@ -35,15 +39,96 @@ def load_data():
 
     return train_indices, validation_indices
 
-if __name__ == "__main__":
-    f = h5py.File('E:/deep-neurons/deep-neurons.hdf5', 'r')
+
+
+def train_gan():
+
+    BATCH_SIZE = 10
+    GENERATOR_PARAMS = 100
+
+    train_index, test_index = load_data()
+
+    f = h5py.File(input_file, 'r')
+
+    images = f['images']
+    labels = f['labels']
+
+    x_train = np.asarray(images)[train_index]
+    y_train = np.asarray(labels)[train_index]
+
+    x_train = (x_train - 127.5) / 127.5
+
+
+    x_test = np.asarray(images)[test_index]
+
+    x_test = (x_test - 127.5) / 127.5
+    y_test = np.asarray(labels)[test_index]
+
+    print(np.max(x_train))
+
+    discriminator = models.discriminator()
+    generator = models.generator()
+    gan = models.gan(generator, discriminator)
+
+    d_opt = Adam(lr=0.000001, beta_1=0.5, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    g_opt = Adam(lr=0.000002, beta_1=0.5, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    gan_opt = Adam(lr=0.00002, beta_1=0.5, beta_2=0.999, epsilon=1e-08, decay=0.0)
+
+    generator.compile(loss='binary_crossentropy', optimizer=g_opt)
+    gan.compile(loss='binary_crossentropy', optimizer=gan_opt)
+
+    discriminator.trainable = True
+    discriminator.compile(loss='binary_crossentropy', optimizer=d_opt)
+
+    noise = np.zeros((BATCH_SIZE, GENERATOR_PARAMS)) # parameters that control generator degrees of freedom
+
+
+    for epoch in range(200):
+        print("Training epoch: " + str(epoch))
+        for index in range(int(x_train.shape[0] / BATCH_SIZE)):
+            for i in range(BATCH_SIZE):
+                noise[i, :] = np.random.uniform(-1, 1, GENERATOR_PARAMS)
+
+            image_batch = np.reshape(x_train[index * BATCH_SIZE:(index + 1) * BATCH_SIZE], (BATCH_SIZE, 128, 128, 1))
+            generated_images = generator.predict(noise, verbose=0)
+
+            # print(np.shape(image_batch))
+            # print(np.shape(generated_images))
+
+            #each batch only real/fake images
+            if epoch % 2 == 0:
+                x = image_batch
+                y = [0.7 + np.random.normal(0, .5 / float(epoch + 1))] * BATCH_SIZE
+            else:
+                x = generated_images
+                y = [0.3 + np.random.normal(0, .5 / float(epoch + 1))] * BATCH_SIZE
+
+            #occasionally flip labels?
+
+            # print("x shape:", np.shape(x))
+            # print("y shape:", np.shape(y))
+
+            d_loss = discriminator.train_on_batch(x, y)
+            for i in range(BATCH_SIZE):
+                noise[i, :] = np.random.uniform(-1, 1, GENERATOR_PARAMS)
+            discriminator.trainable = False
+
+            y_gan = [0.7 + np.random.normal(0, 5/(epoch+1))] * BATCH_SIZE
+            g_loss = gan.train_on_batch(noise, y_gan)
+
+        print("Generator loss:", str(g_loss))
+        print("Discriminator loss:", str(d_loss))
+
+    discriminator.save_weights('discriminator', True)
+    generator.save_weights('generator', True)
+
+def train_discriminator(model):
+    f = h5py.File(input_file, 'r')
 
     train_index, val_index = load_data()
 
     images = f['images']
     labels = f['labels']
-
-    model = models.mlp(3)
 
     sgd = SGD(lr=1e-3, momentum=0.9, decay=1e-6, nesterov=True)
 
@@ -62,4 +147,43 @@ if __name__ == "__main__":
 
     model.fit(x_train, y_train, nb_epoch=200)
 
+def generate():
+    BATCH_SIZE = 2
+    GENERATOR_PARAMS = 100
 
+    opt = Adam(lr=0.0002, beta_1=0.5, beta_2=0.999, epsilon=1e-08, decay=0.0)
+
+    discriminator = models.discriminator()
+    discriminator.compile(loss='binary_crossentropy', optimizer=opt)
+    discriminator.load_weights('discriminator')
+
+    generator = models.generator()
+    generator.compile(loss='binary_crossentropy', optimizer=opt)
+    generator.load_weights('generator')
+
+    noise = np.zeros((BATCH_SIZE * 20, GENERATOR_PARAMS))
+
+    for i in range(BATCH_SIZE * 20):
+        noise[i, :] = np.random.uniform(-1, 1, GENERATOR_PARAMS)
+
+    generated_images = np.zeros((20*BATCH_SIZE, 128, 128, 1))
+
+    for i in range(BATCH_SIZE * 20):
+        generated_images[i, ...] = generator.predict(np.reshape(noise[i, :], (1, GENERATOR_PARAMS)), verbose=1)
+    # d_pret = discriminator.predict(generated_images, verbose=1)
+    #
+    # index = np.arange(0, BATCH_SIZE * 20)
+    # index.resize((BATCH_SIZE * 20, 1))
+    # pre_with_index = list(np.append(d_pret, index, axis=1))
+    # pre_with_index.sort(key=lambda x: x[0], reverse=True)
+    # nice_images = np.zeros((BATCH_SIZE, 1) + (generated_images.shape[2:]), dtype=np.float32)
+    for i in range(len(generated_images)):
+        plt.imshow(generated_images[i, :, :, 0])
+        plt.axis('off')
+        plt.savefig('E:/deep-neurons/generated-' + str(i) + '.png')
+        plt.clf()
+
+if __name__ == "__main__":
+
+    train_gan()
+    generate()
