5bee6bb980abf24c7998dc1eb375ea52ef548a93
mweiss17 <martin.weiss@mail.mcgill.ca>
2018-04-26 12:55:18 -0400

    parameterizable dataset

--- make_dataset.py
+++ make_dataset.py
@@ -2,26 +2,37 @@ import h5py
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-import openslide
+import argparse
 
 from PIL import Image
 
 import os, sys, csv
 
+def build_parser():
+    parser = argparse.ArgumentParser(description="Model for convolution-graph network (CGN)")
+    parser.add_argument('--input-path', default="/Users/martinweiss/code/deep-neurons/labeled/", type=str)
+    return parser
 
-if __name__ == "__main__":
+def parse_args(argv):
+    print argv
+    if type(argv) == list or argv is None:
+        opt = build_parser().parse_args(argv)
+    else:
+        opt = argv
+    return opt
 
-    input_path = 'E:/deep-neurons/'
-    label_file = 'E:/deep-neurons/labels.txt'
+def get_label(filename):
+    return filename.split("/")[-1].split("_")[-1].split(".")[0]
 
-    f = h5py.File('E:/deep-neurons/deep-neurons.hdf5', 'w')
+def main(argv=None):
+    opt = parse_args(argv)
+
+    f = h5py.File(os.getcwd() + '/deep-neurons.hdf5', 'w')
 
     numNeurons = 0
 
     filenames = []
-
-    for root, dirs, files in os.walk(input_path, topdown=False):
+    for root, dirs, files in os.walk(opt.input_path):
         for name in files:
             if '.tif' in name:
                 print(name)
@@ -31,24 +42,19 @@ if __name__ == "__main__":
 
     print(numNeurons, 'neurons')
     images = f.create_dataset('images', shape=(numNeurons, 128, 128), dtype='float32')
-    labels = f.create_dataset('labels', shape=(numNeurons, 3),     dtype='bool')
+    labels = f.create_dataset('labels', shape=(numNeurons, 3), dtype='bool')
 
-    label_list = []
-    csvReader = csv.reader(open(label_file, newline='\n'), delimiter=' ')
-
-    for line in csvReader:
-        label_list.append((line[0], line[1]))
-
-    for i, (filename, label_tuple) in enumerate(zip(filenames, label_list)):
+    for i, filename in enumerate(filenames):
         images[i,...] = Image.open(filename).convert('L')
-
-        print(label_tuple)
-
-        if 'pyr' in label_tuple[1]:
+        label = get_label(filename)
+        if 'pyr' in label:
             labels[i, ...] = [True, False, False]
-        if 'not' in label_tuple[1]:
+        if 'not' in label:
             labels[i,...] = [False, True, False]
-        if 'unk' in label_tuple[1]:
+        if 'unk' in label:
             labels[i,...] = [False, False, True]
 
     f.close()
+
+if __name__ == "__main__":
+    main()
